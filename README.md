dbe8eff73e164275f4547182776986dffb6f8cec
Erin Dahlgren <eedahlgren@gmail.com>
2014-01-26 16:00:19 -0800

    more updates to erin device

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ custom_drivers
 ==============
 
 Simple custom linux kernel drivers
+
+####erin.c: static msg producer and sink device
+
+1. Compile the driver into a module, which can be loaded
+   as a character device.  This will produce a .ko file.
+    $ cd drivers && make
+
+2. Make a new device node
+    $ sudo mknod /dev/erin c 250 0
+    $ sudo chmod a+r+w /dev/erin
+
+3. Insert a module into the linux kernel
+    $ sudo insmod erin.ko
+
+4. Check that the module is loaded
+    $ cat /proc/modules | grep erin
+ 
+5. Check that the module is listed as a character device
+    $ cat /proc/devices | grep erin
+
--- drivers/erin.c
+++ drivers/erin.c
@@ -15,43 +15,68 @@
  *   cleanup method.
  * - using linux/module.h to register the device as a module,
  *   which means it can be loaded into the kernel post-boot.
- *
- * Making a custom character driver can be as simple as:
- *
- * Using your custom character driver can be as simple as:
  */
 
-#include <linux/module.h> // kernel support for initializing and exiting a module.
-                          // From http://linux.die.net/lkmpg:
-                          //     "Modules are pieces of code that can be loaded and 
-                          //     unloaded into the kernel upon demand. They extend
-                          //     the functionality of the kernel without the need to
-                          //     reboot the system. For example, one type of module
-                          //     is the device driver, which allows the kernel to
-                          //     access hardware connected to the system."
-#include <linux/kernel.h> // What's this for?
-#include <linux/fs.h>     // For the file_operations struct, which we use to wrap
-                          // the open, release, read, and write methods of our driver.
+#include <linux/module.h>    // kernel support for initializing and exiting a module.
+                             // From http://linux.die.net/lkmpg:
+                             //     "Modules are pieces of code that can be loaded and 
+                             //     unloaded into the kernel upon demand. They extend
+                             //     the functionality of the kernel without the need to
+                             //     reboot the system. For example, one type of module
+                             //     is the device driver, which allows the kernel to
+                             //     access hardware connected to the system."
+#include <linux/kernel.h>    // What's this for?
+#include <linux/fs.h>        // For the file_operations struct, which we use to wrap
+                             // the open, release, read, and write methods of our driver.
+#include <linux/cdev.h>      // For the character device structure and allocation function.
+#include <linux/uaccess.h>   // For the copy_to_user function, which copies a kernel buffer
+                             // to a userspace buffer.
+#include <linux/semaphore.h> // For creating, holding, and releasing a lock on this
+                             // device, so that only one user can read/write from it at a time.
 
+// Protect the character device file from
+// multiple readers and writers.  This is
+// not very sophisticated.
+struct semaphore sem;
 
+// The static msg this character device gives to consumers.
+static char* MSG = "This is a static message from erin, coming from kernel memory.";
+static int MSG_SIZE = sizeof(MSG);
 
 int erin_open(struct inode *inode, struct file *f)
 {
-  // 
+  // Try to acquire the semaphore, but stop trying
+  // if the user sends a signal like ^C.
+  if (down_interruptible(&sem)) {
+    printk(KERN_INFO "/dev/erin: could not hold semaphore\n");
+    return -1;
+  }
+  printk(KERN_INFO "/dev/erin: opened\n");
+  return 0;
 }
 
 int erin_release(struct inode *inode, struct file *f)
 {
-  //
+  // Release the semaphore so other users of this
+  // device can open it.
+  up(&sem);
+  printk(KERN_INFO "/dev/erin: closed\n");
+  return 0;
 }
 
 ssize_t erin_read(struct file *f, char *buf, size_t count, loff_t *offp)
-//
 {
+  // Called when a process tries to read from this device.
+  // If the user is to get any data, we need to copy
+  // any kernel buffers here to the user space buffer buf.
+  return copy_to_user(buf, MSG, MSG_SIZE);
 }
 
-ssize_t erin_write(struct file *f, char *buf, size_t count, loff_t *offp)
+ssize_t erin_write(struct file *f, const char *buf, size_t count, loff_t *offp)
 {
+  // Consumes data like a sink, doing nothing with it.
+  printk(KERN_INFO "/dev/erin: consumed %ld bytes\n", count);
+  return count;
 }
 
 /*
@@ -76,17 +101,13 @@ dev_t device_numbers;
 // be used to cleanup the device_number above.
 static int major_number;
 
-// 
-
 /*
  * Initialize the character device.
- *
- * Steps include:
- * - 
- * -
  */
 int erin_init(void)
 {
+  int ret;
+
   // obtain a standalone cdev structure at runtime.
   character_device = cdev_alloc();
   // this character device uses the erin driver file operations 
@@ -98,7 +119,6 @@ int erin_init(void)
   // dynamically ask the kernel for a device number for this
   // character device. Every linux device has a major and
   // minor number.
-  int ret;
   ret = alloc_chrdev_region(&device_numbers, 0, 1, "erin");
   if (ret < 0) {
     printk("Oh shit. Major number allocation failed\n");
@@ -120,7 +140,7 @@ int erin_init(void)
 /*
  * Cleanup the character device.
  */
-void erin_cleaup(void)
+void erin_cleanup(void)
 {
   //
   cdev_del(character_device);
@@ -132,6 +152,6 @@ void erin_cleaup(void)
  * Make the driver a kernel module,
  * which can be loaded post-boot.
  */
-MODULE_LICENSE("GPL")
-module_init(erin_init)
-module_exit(erin_cleanup)
+MODULE_LICENSE("GPL");
+module_init(erin_init);
+module_exit(erin_cleanup);
